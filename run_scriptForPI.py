d2afc2b6ffa98c563d5a30454c6aa87f08716447
Toshiki Saito <toshiki.saito1005@gmail.com>
2018-02-12 13:44:39 +0100

    add new file

---
+++ run_scriptForPI.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import glob
+
+dir_data = glob.glob("science_*/group.*/member.*/")
+dir_current = os.getcwd()
+
+for i in range(len(dir_data)):
+    os.chdir(dir_data[i])
+    done = glob.glob("calibrated/")
+    if not done:
+        print("run scriptForPI.py for " + dir_data[i].split("/")[0])
+        os.chdir("script")
+        execfile(glob.glob("*scriptForPI.py")[0])
+    else:
+        print("skip " + dir_data[i].split("/")[0])
+    os.chdir(dir_current)
+
+
